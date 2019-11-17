a0124c81eaa4973c8ceb967b131497e788712117
nuogel <42196367+nuogel@users.noreply.github.com>
2019-11-17 20:23:57 +0800

    0

---
+++ evaSys/mAP_master/main_map.py
@@ -0,0 +1,9 @@
+from evaSys.mAP_master.MAP import main
+
+
+def _convert_xml2txt():
+    ...
+
+
+if __name__ == '__main__':
+    main()
---
+++ evaSys/score.py
@@ -0,0 +1,6 @@
+from evaSys.F1Score.f1score import F1Score
+from evaSys.ASR_WER.wer import ASR_SCORE
+
+
+Score = {'img': F1Score,
+         'ASR': ASR_SCORE}
